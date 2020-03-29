8834d1dc7f792b4c65ea2e0ed86ca8ba0b039730
shahar05 <shram92@gmail.com>
2020-03-29 22:24:43 +0300

    mongo url changed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var myTrips;
 
 const app = express();
 try {
-    mongoose.connect('mongodb://localhost:27017/trip_app', { useNewUrlParser: true });    
+    mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds031571.mlab.com:31571/trips', { useNewUrlParser: true });    
 } catch (error) {
     console.error("failed connectd to db!!");
     console.log(error);
